b0f21c7cfb4425ab315837b1314e8672a07e8add
Hriday Pradhan <37269407+hridaypradhan@users.noreply.github.com>
2020-12-14 15:49:04 +0530

    Create q2.kt

---
+++ week1/q2.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println(findMax(1,2,3))
+    println(findMax(97,206,154))
+}
+
+fun findMax(number1 : Int, number2 : Int, number3 : Int) : Int {
+    return maxOf(number1,number2, number3)
+}
